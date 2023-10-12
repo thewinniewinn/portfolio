4a0f7d81c15f16f8e8b91fa7b4416cea80c7a4a0
winniewin-tn <dev.winniewin@gmail.com>
2023-10-12 23:52:04 +0800

    Added routes and NavBar component

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./index.css";
+import Home from "./routes/Home"
+import Blogs from "./routes/Blogs"
+import Projects from "./routes/Projects"
+import Resources from "./routes/Resources"
+
+import {Routes, Route} from "react-router-dom"
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/blogs" element={<Blogs/>}></Route>
+      <Route path="/projects" element={<Projects/>}></Route>
+      <Route path="/resources" element={<Resources/>}></Route>
+    </Routes>
+    </>
+  );
+}
+
+export default App;
